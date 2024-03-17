26c10a7a10991f9646aa4507df038ffac7600905
aafif <aafifpratama@gmail.com>
2024-03-17 19:19:33 +0700

    [Ardena] Codelab Corountine Flow - Reactive Programming

--- app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/RemoteDataSource.kt
@@ -2,21 +2,13 @@ package com.dicoding.tourismapp.core.data.source.remote
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiResponse
 import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
-import com.dicoding.tourismapp.core.data.source.remote.response.ListTourismResponse
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 @SuppressLint("CheckResult")
 class RemoteDataSource private constructor(private val apiService: ApiService) {
@@ -30,25 +22,22 @@ class RemoteDataSource private constructor(private val apiService: ApiService) {
             }
     }
 
-    fun getAllTourism(): Flowable<ApiResponse<List<TourismResponse>>> {
-        val resultData = PublishSubject.create<ApiResponse<List<TourismResponse>>>()
+    fun getAllTourism(): Flow<ApiResponse<List<TourismResponse>>> {
 
-        //get data from local json
-        val client = apiService.getList()
-
-        client
-            .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .take(1)
-            .subscribe ({ response ->
+        return flow {
+            try {
+                val response = apiService.getList()
                 val dataArray = response.places
-                resultData.onNext(if (dataArray.isNotEmpty()) ApiResponse.Success(dataArray) else ApiResponse.Empty)
-            }, {error ->
-                resultData.onNext(ApiResponse.Error(error.message.toString()))
-                Log.e("RemoteDataSource", error.toString())
-            })
-
-        return resultData.toFlowable(BackpressureStrategy.BUFFER)
+                if (dataArray.isNotEmpty()) {
+                    emit(ApiResponse.Success(response.places))
+                } else {
+                    emit(ApiResponse.Empty)
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+                Log.e("RemoteDataSource", e.toString())
+            }
+        }.flowOn(Dispatchers.IO)
     }
 }
 
